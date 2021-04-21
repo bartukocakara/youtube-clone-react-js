a592b7b01495be6c3429ecdc5583bac180f34d36
bartukocakara <kocakarabartu@gmail.com>
2021-04-21 22:20:25 +0300

    CI READY

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import App from './App';
+import ReactDOM from 'react-dom'
+import React from 'react'
+import App from './App'
 import { Provider } from 'react-redux'
-import store from './redux/store';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-lazy-load-image-component/src/effects/blur.css'
 import { BrowserRouter as Router } from 'react-router-dom'
-import 'react-lazy-load-image-component/src/effects/blur.css';
 
-
-import "./_base.scss";
-
+import './_base.scss'
+import store from './redux/store'
 
 ReactDOM.render(
-<Provider store={store}>
-    <Router>
-        <App/>
-    </Router>
-</Provider>,
+   <Provider store={store}>
+      <Router>
+         <App />
+      </Router>
+   </Provider>,
 
-
-document.getElementById("root"));
+   document.getElementById('root')
+)
